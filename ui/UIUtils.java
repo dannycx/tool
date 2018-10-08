ade35a909d767e6db1fe3a7ec4d65eb869e02408
DannyCX <33610732+dannycx@users.noreply.github.com>
2018-10-08 21:16:04 +0800

    Update UIUtils.java

--- ui/UIUtils.java
+++ ui/UIUtils.java
@@ -23,46 +23,10 @@ public class UIUtils {
     //获取主线程id
     public static int getUiThreadId(){return SaleApplication.getUiThreadId();}
 
-    //获取字符串
-    public static String getString(int resId){return getContext().getResources().getString(resId);}
+   
 
-    //获取字符串数组
-    public static String[] getStringArray(int resId){return getContext().getResources().getStringArray(resId);}
 
-    //获取图片
-    public static Drawable getDrawable(int resId){return getContext().getResources().getDrawable(resId);}
-
-    //获取颜色
-    public static int getColor(int resId){return getContext().getResources().getColor(resId);}
-
-    //根据id获取颜色的状态选择器
-    public static ColorStateList getColorStateList(int resId) {
-        return getContext().getResources().getColorStateList(resId);
-    }
-
-    //获取像素值
-    public static int getDimen(int resId){return getContext().getResources().getDimensionPixelSize(resId);}
-
-    //获取系统认为最小滑动距离TouchSlop,当小于该值时,忽略滑动(8dp)
-    public static int getTouchSlop(Context context){
-        //mSlop = ViewConfiguration.getWindowTouchSlop();
-        return ViewConfiguration.get(context).getScaledTouchSlop();
-    }
-
-    //dp-px
-    public static int dp2px(float dp){
-        float density = getContext().getResources().getDisplayMetrics().density;
-        return (int) (dp*density+0.5f);
-    }
-
-    //px-dp
-    public static float px2dp(int px){
-        float density = getContext().getResources().getDisplayMetrics().density;
-        return px/density;
-    }
-
-    //加载布局文件
-    public static View inflate(int resId){return View.inflate(getContext(),resId,null);}
+    
 
     //判断是否运行在主线程
     public static boolean isRunningInUiThread(){
